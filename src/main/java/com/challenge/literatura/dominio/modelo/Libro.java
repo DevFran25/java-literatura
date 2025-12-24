ddfd63f56eb571033cb69e716d734f1ea66a5f45
Francisco Collantes <developerfran25@gmail.com>
2025-12-24 10:28:45 -0500

    Se agrego la clase libro

--- src/main/java/com/challenge/literatura/dominio/modelo/Libro.java
+++ src/main/java/com/challenge/literatura/dominio/modelo/Libro.java
@@ -1,4 +1,67 @@
 package com.challenge.literatura.dominio.modelo;
+import jakarta.persistence.*;
+
+@Entity
 
 public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long libroId;
+
+    @Column(nullable = false)
+    private String nombreTitulo;
+
+    @Column(length = 10)
+    private String lengua;
+
+    @Column(name = "total_descargas")
+    private Integer totalDescargas;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "autor_id", nullable = false)
+    private Autor autor;
+
+    public Long getLibroId() {
+        return libroId;
+    }
+
+    public String getNombreTitulo() {
+        return nombreTitulo;
+    }
+
+    public void setNombreTitulo(String nombreTitulo) {
+        this.nombreTitulo = nombreTitulo;
+    }
+
+    public String getLengua() {
+        return lengua;
+    }
+
+    public void setLengua(String lengua) {
+        this.lengua = lengua;
+    }
+
+    public Integer getTotalDescargas() {
+        return totalDescargas;
+    }
+
+    public void setTotalDescargas(Integer totalDescargas) {
+        this.totalDescargas = totalDescargas;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro: " + nombreTitulo +
+                " | Idioma: " + lengua +
+                " | Descargas: " + (totalDescargas != null ? totalDescargas : 0) +
+                " | Autor: " + (autor != null ? autor.getNombreCompleto() : "N/A");
+    }
 }
